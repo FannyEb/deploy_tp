ef61ef6737cfb5037c658a96687ae8990f610521
Fanny <feber@capcod.eu>
2022-12-26 23:55:31 +0100

    correct path

--- deploy/api/index.php
+++ deploy/api/index.php
@@ -6,7 +6,7 @@ use Tuupola\Middleware\HttpBasicAuthentication;
 use \Firebase\JWT\JWT;
 require __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../bootstrap.php';
-require_once __DIR__ . '/../model.php';
+require_once __DIR__ . '/model.php';
  
 const JWT_SECRET = "makey1234567";
 
