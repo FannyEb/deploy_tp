9cb00c502dd7b17734528f19915424ba9c3cff72
Fanny <feber@capcod.eu>
2023-01-07 16:23:31 +0100

    add includes

--- deploy/api/index.php
+++ deploy/api/index.php
@@ -6,7 +6,8 @@ use Tuupola\Middleware\HttpBasicAuthentication;
 use \Firebase\JWT\JWT;
 require __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../bootstrap.php';
-require_once __DIR__ . '/model.php';
+require_once __DIR__ . '/models/Client.php';
+require_once __DIR__ . '/models/Product.php';
  
 const JWT_SECRET = "makey1234567";
 
